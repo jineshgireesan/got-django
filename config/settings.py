df48a0959e207ed82dd46764611ef6d31be790f4
zoophystry-test <zoophystry.test@gmail.com>
2026-02-16 20:33:12 +0530

    connect postgres database

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
