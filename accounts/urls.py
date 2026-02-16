4b4055f98a05b466b10561921c902c0607c7f143
zoophystry-test <zoophystry.test@gmail.com>
2026-02-16 14:06:22 +0530

    fix admin url location

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
 from . import views
-from accounts.views import create_admin
+
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('characters/', views.characters, name='characters'),
-    path('logout/', views.logout_view, name='logout'),
-    path('create-admin/', views.create_admin),
-]
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('create-admin/', create_admin),   # ADD THIS
-    path('', include('accounts.urls')),
+    path('logout/', views.logout_view, name='logout')
 ]
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import create_admin
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create-admin/', create_admin),
     path('', include('accounts.urls')),
 ]
 
