2e7ed51ef3bca47460784c063e79310807842e1c
zoophystry-test <zoophystry.test@gmail.com>
2026-02-17 13:56:38 +0530

    fix admin

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import create_admin
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('create-admin/', create_admin),
     path('', include('accounts.urls')),
 ]
 
-# This line makes images work in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
